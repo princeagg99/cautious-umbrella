3a45f743dbee299db995e5194ea07de2e25cc3b9
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-01-25 18:08:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ if button1:
     st.session_state.button_clicked = True
     st.write("abcdef")
 
-# Button 2
-button2 = st.button("Button 2")
-if button2:
-    st.session_state.button_clicked = True
-    st.write("abcdefasfas")
+    # Button 2
+    button2 = st.button("Button 2")
+    if button2:
+        st.session_state.button_clicked = True
+        st.write("abcdefasfas")
 
 # Display the current state
 st.write(f"Button Clicked: {st.session_state.button_clicked}")
