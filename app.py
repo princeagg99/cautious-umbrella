bede35e8882e047cab397543cf86ea10febae31e
princeagg99 <147048434+princeagg99@users.noreply.github.com>
2024-01-25 16:50:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+if 'button_clicked' not in st.session_state:
+    st.session_state.button_clicked = False
+
+# Button 1
+button1 = st.button("Button 1")
+if button1:
+    st.session_state.button_clicked = True
+
+# Button 2
+button2 = st.button("Button 2")
+if button2:
+    st.session_state.button_clicked = True
+
+# Display the current state
+st.write(f"Button Clicked: {st.session_state.button_clicked}")
